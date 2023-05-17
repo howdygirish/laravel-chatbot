199c6975a6eaf77781d5633275118f824056a77b
kemuritechnology <girish@kemuri.in>
2023-05-17 16:03:25 +0530

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// https://www.tutsmake.com/laravel-10-vue-js-crud-tutorial-example/
+// https://laraveldaily.com/post/laravel-8-vue-3-crud-composition-api
